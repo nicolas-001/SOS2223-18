70ff6d97e0994674a6f1d52a644db5ffe3fdebba
Nicolás Jiménez Olmedo <nicjimolm@us.es>
2023-03-09 18:27:00 +0100

    .

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 var express = require("express");
 var cool = require("cool-ascii-faces");
-
 var app = express();
 var port = process.env.PORT || 3000
-
-
 
 
 //Ruta para caras ascii
 app.get("/cool", (request, response) => {
     response.send(cool());
     console.log("New request");  
-
 });
 
- 
 //Rutas
 
     app.use(require('./routes/index-NJO')) ;
     app.use(require('./routes/index-arm')) ;
     app.use(require('./routes/index-ala')) ;
-
-
-
-
-
 
 
     app.listen(port, () => {
